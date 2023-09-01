cbe5ea734b52422d0e6882ee807b0e00dcfd2d25
Anthony Ramos <ramosanthony@gmail.com>
2023-09-01 11:54:25 -0400

    Roof CRUD created

--- frontend/src/components/Basement.js
+++ frontend/src/components/Basement.js
@@ -1,28 +1,114 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import CapstoneApi from "../api";
+import BasementForm from "./BasementForm";
 
 export default function Basement(props) {
   const { householdId } = props;
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const [basement, setBasement] = useState({});
+  const [isNew, setIsNew] = useState(false);
+  const [formData, setFormData] = useState();
+
+  async function getBasement() {
+    let basement = await CapstoneApi.getBasement(householdId);
+    setBasement(basement);
+    if (basement === "") {
+      setIsNew(true);
+    }
+    setIsLoading(false);
+  }
+
+  async function updateBasement(data) {
+    let basement = await CapstoneApi.updateBasement(data);
+    setRoof(basement);
+    setIsLoading(false);
+    console.log("Basement Updated: " + basement.id);
+  }
+
+  async function createBasement(data) {
+    let basement = await CapstoneApi.createBasement(data);
+    setRoof(basement);
+    setIsLoading(false);
+    setIsNew(false);
+    console.log("Basement Created: " + basement.id);
+  }
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((fdata) => ({
+      ...fdata,
+      [name]: value,
+    }));
+  };
+
+  const checkboxChangeHandler = (e) => {
+    const { name, checked } = e.target;
+    setFormData((fdata) => ({
+      ...fdata,
+      [name]: checked ? checked : false,
+    }));
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    let data = {
+      id: isNew ? null : formData.id,
+      householdId: householdId,
+      installationDate: formData.installationDate,
+      invoicePhoto: formData.invoicePhoto,
+      hasBeenReplaced: formData.hasBeenReplaced,
+      hadExistingMaterialRemoved: formData.hadExistingMaterialRemoved,
+      hasPreexistingLeaks: formData.hasPreexistingLeaks,
+      hasRainwaterProblems: formData.hasRainwaterProblems,
+      notes: formData.notes,
+    };
+
+    try {
+      if (isNew) {
+        createRoof(data);
+      } else {
+        updateRoof(data);
+      }
+    } catch (errors) {
+      setIsLoading(false);
+      return setFormData((data) => ({ ...data, errors }));
+    }
+  };
+
+  useEffect(() => {
+    setFormData({
+      id: roof.id,
+      householdId: householdId ?? null,
+      installationDate: roof.installationDate ?? "",
+      invoicePhoto: roof.invoicePhoto ?? null,
+      hasBeenReplaced: roof.hasBeenReplaced ?? false,
+      hadExistingMaterialRemoved: roof.hadExistingMaterialRemoved ?? false,
+      hasPreexistingLeaks: roof.hasPreexistingLeaks ?? false,
+      hasRainwaterProblems: roof.hasRainwaterProblems ?? false,
+      notes: roof.notes ?? "",
+      errors: [],
+    });
+  }, [roof]);
+
+  useEffect(() => {
+    getRoof();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading &hellip;</p>;
+  }
   return (
     <Card className="mb-3">
       <Card.Body>
-        <Card.Title>Some Basement data here {householdId}</Card.Title>
-        <Card.Text>
-          More info
-          <br />
-          More info
-          <Button
-            className="d-block ml-auto"
-            //disabled={appliedFor}
-            //onClick={() => applyHandler(id)}
-            variant="danger"
-            type="button"
-          >
-            Button text here
-          </Button>
-        </Card.Text>
+        <RoofForm
+          submitHandler={submitHandler}
+          changeHandler={changeHandler}
+          checkboxChangeHandler={checkboxChangeHandler}
+          formData={formData}
+          buttonText={"Save Changes"}
+        />
       </Card.Body>
     </Card>
   );
